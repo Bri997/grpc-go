cf6215b28efed80a23046b05e607e8e9389432db
brian <brian.musial@gmail.com>
2019-12-10 17:08:15 -0500

    calculator grpc server

---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/bri997/grpc-go-course/greet/hands-on/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+
+	fmt.Printf("Cal function was invoked with %v\n", req)
+	number1 := req.Num1
+	number2 := req.Num2
+	sum := number1 + number2
+	res := &calculatorpb.SumResponse{
+		Result: sum,
+	}
+	return res, nil
+}
+
+func main() {
+	fmt.Println("hi Cal")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("fail %v", err)
+
+	}
+
+	s := grpc.NewServer()
+
+	calculatorpb.RegisterCalculateServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server %v:", err)
+	}
+}
